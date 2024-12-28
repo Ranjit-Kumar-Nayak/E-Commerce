bccdde355fc0b075fe0d4725aee94be90f0f6bfe
ranjit <ranjitkumarnayak420@gmail.com>
2024-12-28 21:36:55 +0530

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>Get In Touch </p>
                 <ul className='flex flex-col gap-1 text-gray-600a'>
-                    <li>+1-212-456-7890</li>
+                    <li>+1-212-456-78900</li>
                     <li>contact@forever.com</li>
                 </ul>
             </div>
